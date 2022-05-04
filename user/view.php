42786181537374828a6f355759cb590af70c88a9
SafetyZone <81029510+Muhammedska@users.noreply.github.com>
2022-05-04 21:33:02 +0300

    viewer added

--- user/view.php
+++ user/view.php
@@ -39,11 +39,7 @@ while ($row = $res->fetchArray()) {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="../assets/fonts/fontawesome-all.min.css">
     <link rel="shortcut icon" href="../favicon.svg" type="image/x-icon">
-    <style>
-        html {
-            scroll-behavior: smooth;
-        }
-    </style>
+
 </head>
 
 <body id="page-top">
@@ -195,141 +191,44 @@ while ($row = $res->fetchArray()) {
                     }
                     ?>
 
-                    <h3 class="text-dark mb-4">Video Çözümler</h3>
+                    <div>
+                        <h3 class="text-dark mb-4"><?php echo strtoupper($_GET['name']) . '&nbsp;<i class="fas fa-angle-right"></i>&nbsp;' . $_GET["examname"]; ?></h3>
+                    </div>
                     <div class="row mb-3">
-                        <div class="col-lg-4">
+                        <div class="col-lg-8">
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="text-primary font-weight-bold m-0">Denemeler</h6>
-                                </div>
-
-                                <div class="card-body" style="max-height:400px;overflow-y:auto;">
-                                    <?php
-                                    $examdir = "../src/video/exams/";
-                                    $folders = scandir($examdir);
-                                    array_reverse($folders);
-                                    $firstexam = "";
-                                    $istakefe = false;
-                                    for ($i = 0; $i < count($folders); $i++) {
-                                        if (!($folders[$i] == '.') && !($folders[$i] == '..' && !($folders[$i] == "active.txt"))) {
-                                            if (is_file($examdir . $folders[$i] . "/active.txt")) {
-                                                $activedir = fgets(fopen($examdir . $folders[$i] . "/active.txt","r"));
-                                                if ($activedir == "1") {
-                                                    echo "<a class='btn btn-primary container text-center mb-3' href='./video.php?examname={$folders[$i]}'>{$folders[$i]}</a>";
-                                                    if ($istakefe == false) {
-                                                        $firstexam = $folders[$i];
-                                                        $istakefe = true;
-                                                    }
-                                                }
-                                            } else {
-                                                $secfile = fopen($examdir . $folders[$i] . "/active.txt","w");
-                                                fwrite($secfile,"0");
-                                                fclose($secfile);
-                                            }
-                                        }
-                                    }
-                                    if (empty($_GET['examname'])) {                                        
-                                        $dirname = $examdir . $firstexam . "/";
-                                        $examname = $firstexam;
-                                    } else {
-                                        $dirname = $examdir . $_GET['examname'] . "/";
-                                        $examname = $_GET['examname'] ;
-                                    }
-                                    
-                                    ?>
-                                </div>
-                            </div>
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="text-primary font-weight-bold m-0">Hızlı Ulaşım</h6>
+                                    <h6 class="text-primary font-weight-bold m-0">Video</h6>
                                 </div>
 
                                 <div class="card-body">
-                                    <div class="container flex-wrap text-center">
-                                        <a href="#matematik" class="btn mb-3 btn-primary" style="width:120px">Matematik</a>
-                                        <a href="#fizik" class="btn mb-3 btn-primary" style="width:120px">Fizik</a>
-                                        <a href="#kimya" class="btn mb-3 btn-primary" style="width:120px">Kimya</a>
-                                        <a href="#biyoloji" class="btn mb-3 btn-primary" style="width:120px">Biyoloji</a>
-                                    </div>
+                                    <video controls style="width:100%;" src="<?php echo $_GET['dir'] . $_GET['name'] . '/' . $_GET['file'] ?>"></video>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="<?php echo $_GET['dir'] . $_GET['name'] . '/' . $_GET['file'] ?>" class="btn btn-primary" download><i class="fa fa-download" aria-hidden="true"></i>&nbsp;&nbsp;Videoyu İndir</a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-8">
-
-                            <div class="row">
-                                <div class="col">
-                                    <div class="card shadow mb-3" id="matematik">
-                                        <div class="card-header py-3">
-                                            <p class="text-primary m-0 font-weight-bold">Matematik</p>
-                                        </div>
-                                        <div class="card-body container p-2">
-                                            <?PHP
-                                            $finder = scandir($dirname . "matematik");
-                                            array_diff($finder, [".", ".."]);
-                                            for ($i = 0; $i < count($finder); $i++) {
-                                                $checkdir = $dirname . "/matematik" . "/" . $finder[$i];
-                                                if (is_file($checkdir)) {
-                                                    $link = str_replace('.jpeg', "", $finder[$i]);
-                                                    echo "<a class='alert btn-primary btn text-left ' style='width:100%; position:relative;' href='./view.php?file={$finder[$i]}&name=matematik&dir={$dirname}&examname={$examname}'><i class='fa fa-arrow-right'></i>&nbsp; {$link} &nbsp;</a>";
-                                                }
-                                            }
-                                            ?>
-                                        </div>
-                                    </div>
-                                    <div class="card shadow mb-3" id="fizik">
-                                        <div class="card-header py-3">
-                                            <p class="text-primary m-0 font-weight-bold">Fizik</p>
-                                        </div>
-                                        <div class="card-body container p-2">
-                                            <?PHP
-                                            $finder = scandir($dirname . "fizik");
-                                            array_diff($finder, [".", ".."]);
-                                            for ($i = 0; $i < count($finder); $i++) {
-                                                $checkdir = $dirname . "/fizik" . "/" . $finder[$i];
-                                                if (is_file($checkdir)) {
-                                                    $link = str_replace('.jpeg', "", $finder[$i]);
-                                                    echo "<a class='alert btn-primary btn text-left' style='width:100%; position:relative;' href='./view.php?file={$finder[$i]}&name=fizik&dir={$dirname}&examname={$examname}'><i class='fa fa-arrow-right'></i>&nbsp; {$link} &nbsp;</a>";
-                                                }
-                                            }
-                                            ?>
-                                        </div>
-                                    </div>
-                                    <div class="card shadow mb-3" id="kimya">
-                                        <div class="card-header py-3">
-                                            <p class="text-primary m-0 font-weight-bold">Kimya</p>
-                                        </div>
-                                        <div class="card-body container p-2">
-                                            <?PHP
-                                            $finder = scandir($dirname . "kimya");
-                                            array_diff($finder, [".", ".."]);
-                                            for ($i = 0; $i < count($finder); $i++) {
-                                                $checkdir = $dirname . "/kimya" . "/" . $finder[$i];
-                                                if (is_file($checkdir)) {
-                                                    $link = str_replace('.jpeg', "", $finder[$i]);
-                                                    echo "<a class='alert btn-primary btn text-left' style='width:100%; position:relative;' href='./view.php?file={$finder[$i]}&name=kimya&dir={$dirname}&examname={$examname}'><i class='fa fa-arrow-right'></i>&nbsp; {$link} &nbsp;</a>";
-                                                }
-                                            }
-                                            ?>
-                                        </div>
-                                    </div>
-                                    <div class="card shadow" id="biyoloji">
-                                        <div class="card-header py-3">
-                                            <p class="text-primary m-0 font-weight-bold">Biyoloji</p>
-                                        </div>
-                                        <div class="card-body container p-2">
-                                            <?PHP
-                                            $finder = scandir($dirname . "biyoloji");
-                                            array_diff($finder, [".", ".."]);
-                                            for ($i = 0; $i < count($finder); $i++) {
-                                                $checkdir = $dirname . "/biyoloji" . "/" . $finder[$i];
-                                                if (is_file($checkdir)) {
-                                                    $link = str_replace('.jpeg', "", $finder[$i]);
-                                                    echo "<a class='alert btn-primary btn text-left' style='width:100%; position:relative;' href='./view.php?file={$finder[$i]}&name=biyoloji&dir={$dirname}&examname={$examname}'><i class='fa fa-arrow-right'></i>&nbsp; {$link} &nbsp;</a>";
-                                                }
-                                            }
-                                            ?>
-                                        </div>
-                                    </div>
+                        <div class="col-lg-4">
+                            <div class="card shadow">
+                                <div class="card-header py-3">
+                                    <p class="text-primary m-0 font-weight-bold"><?php echo strtoupper($_GET['name'])?></p>
+                                </div>
+                                <div class="card-body container p-2">
+                                    <?PHP
+                                    $dirname = $_GET['dir'];
+                                    $lection = $_GET['name'];
+                                    $examname = $_GET['examname'];
+                                    $finder = scandir($dirname . "{$lection}");
+                                    array_diff($finder, [".", ".."]);
+                                    for ($i = 0; $i < count($finder); $i++) {
+                                        $checkdir = $dirname . "/{$lection}" . "/" . $finder[$i];
+                                        if (is_file($checkdir)) {
+                                            $link = str_replace('.mp4', "", $finder[$i]);
+                                            echo "<a class='alert btn-primary btn text-left' style='width:100%; position:relative;' href='./view.php?file={$finder[$i]}&name={$lection}&dir={$dirname}&examname={$examname}'><i class='fa fa-arrow-right'></i>&nbsp; {$link} &nbsp;</a>";
+                                        }
+                                    }
+                                    ?>
                                 </div>
                             </div>
                         </div>
