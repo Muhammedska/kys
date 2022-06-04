b778dd050874329347b3327afc15f60a33d3438a
SafetyZone <81029510+Muhammedska@users.noreply.github.com>
2022-06-04 10:38:21 +0300

    added notify creation side

--- admin/admin.php
+++ admin/admin.php
@@ -172,7 +172,7 @@ $folders = array_diff($folders, [".", ".."]);
                                             </a>
                                             <div class="dropdown-menu shadow dropdown-menu-right animated--fade-in">
                                                 <p class="text-center dropdown-header">İşlemler:</p>
-                                                <a class="dropdown-item" href="#">&nbsp;Sil</a>                                                
+                                                <a class="dropdown-item" href="#">&nbsp;Sil</a>
                                                 <div class="dropdown-divider"></div><a class="dropdown-item" href="#">&nbsp;Something else here</a>
                                             </div>
 
@@ -189,7 +189,7 @@ $folders = array_diff($folders, [".", ".."]);
                                             </a>
                                             <div class="dropdown-menu shadow dropdown-menu-right animated--fade-in">
                                                 <p class="text-center dropdown-header">İşlemler:</p>
-                                                <a class="dropdown-item" href="#">&nbsp;Sil</a>                                                
+                                                <a class="dropdown-item" href="#">&nbsp;Sil</a>
                                                 <div class="dropdown-divider"></div><a class="dropdown-item" href="#">&nbsp;Something else here</a>
                                             </div>
 
@@ -204,19 +204,25 @@ $folders = array_diff($folders, [".", ".."]);
                         </div>
                         <div class="col-lg-5 col-xl-4">
                             <div class="card shadow mb-4">
-                                <div class="card-header d-flex justify-content-between align-items-center">
-                                    <h6 class="text-primary font-weight-bold m-0">Revenue Sources</h6>
-                                    <div class="dropdown no-arrow">
-                                        <button class="btn btn-link btn-sm dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button"><i class="fas fa-ellipsis-v text-gray-400"></i></button>
-                                        <div class="dropdown-menu shadow dropdown-menu-right animated--fade-in">
-                                            <p class="text-center dropdown-header">dropdown header:</p><a class="dropdown-item" href="#">&nbsp;Action</a><a class="dropdown-item" href="#">&nbsp;Another action</a>
-                                            <div class="dropdown-divider"></div><a class="dropdown-item" href="#">&nbsp;Something else here</a>
-                                        </div>
-                                    </div>
+                                <div class="card-header py-3">
+                                    <h6 class="text-primary font-weight-bold m-0">Bildirim Oluştur</h6>
                                 </div>
                                 <div class="card-body">
-                                    <div class="chart-area"><canvas data-bs-chart="{&quot;type&quot;:&quot;doughnut&quot;,&quot;data&quot;:{&quot;labels&quot;:[&quot;Direct&quot;,&quot;Social&quot;,&quot;Referral&quot;],&quot;datasets&quot;:[{&quot;label&quot;:&quot;&quot;,&quot;backgroundColor&quot;:[&quot;#4e73df&quot;,&quot;#1cc88a&quot;,&quot;#36b9cc&quot;],&quot;borderColor&quot;:[&quot;#ffffff&quot;,&quot;#ffffff&quot;,&quot;#ffffff&quot;],&quot;data&quot;:[&quot;50&quot;,&quot;30&quot;,&quot;15&quot;]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:false},&quot;title&quot;:{}}}"></canvas></div>
-                                    <div class="text-center small mt-4"><span class="mr-2"><i class="fas fa-circle text-primary"></i>&nbsp;Direct</span><span class="mr-2"><i class="fas fa-circle text-success"></i>&nbsp;Social</span><span class="mr-2"><i class="fas fa-circle text-info"></i>&nbsp;Refferal</span></div>
+                                    <form action="./gear.php" method="get">
+                                        <label for="notifytext">Bildirim Metni :</label>
+                                        <textarea name="notifytext" id="notifytext" cols="30" rows="5" class="form-control" placeholder="Bildirim metni giriniz" required></textarea>
+                                        <div class="form-check flex-wrap container text-center">
+                                            <label class="form-check-label mb-2 my-3 " style="margin-right:30px;">
+                                                <input type="radio" class="form-check-input" name="notifyfor" id="notifytypestudent" value="student" checked>
+                                                Öğrenci
+                                            </label>
+                                            <label class="form-check-label mb-2 my-3 " style="margin-right:30px;">
+                                                <input type="radio" class="form-check-input" name="notifyfor" id="notifytypeteacher" value="teacher">
+                                                Öğretmen
+                                            </label>                                           
+                                        </div>
+                                        <button class="btn btn-primary" type="submit">Bildirim Oluştur</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
