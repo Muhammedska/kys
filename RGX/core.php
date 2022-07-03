fdc2a813d8a9ac70e3afc1c773b4e92076e276af
SafetyZone <81029510+Muhammedska@users.noreply.github.com>
2022-07-03 23:41:25 +0300

    desktop core add

--- RGX/core.php
+++ RGX/core.php
@@ -31,5 +31,47 @@ while ($row = $results->fetchArray()) {
     array_push($datam, array($row['ID'], $row['name'], $row['lesson'], $row['pp']));
 };
 
+$sql = "SELECT * FROM app WHERE var='token'";
+$results = $db->prepare($sql);
+$res = $results->execute();
+$row = $res->fetchArray(SQLITE3_NUM);
+$token = $row[1];
 
-?>
+if ($_GET['token'] == $token) {
+    if ($_GET['type'] == 'add') {
+        if ($_GET['t'] == 'student') {
+            $sql = "INSERT INTO student (ID, name, grade, pp) VALUES ('" . $_GET['id'] . "', '" . $_GET['name'] . "', '" . $_GET['grade'] . "', '" . 'franko' . "')";
+            $results = $db->prepare($sql);
+            $res = $results->execute();
+            echo 'success';
+        } else if ($_GET['t'] == 'teacher') {
+            $sql = "INSERT INTO teacher (ID, name, lesson, pp) VALUES ('" . $_GET['id'] . "', '" . $_GET['name'] . "', '" . $_GET['lesson'] . "', '" . 'franko' . "')";
+            $results = $db->prepare($sql);
+            $res = $results->execute();
+            echo 'success';
+        } else {
+            echo 'error';
+        }
+    } else if ($_GET['type'] == 'getlog') {
+        $sql = "SELECT * FROM log";
+        $results = $db->query($sql);
+        //$results = $res->execute();
+        $notify = [];
+        while ($row = $results->fetchArray()) {
+            array_push($notify, array($row['mission'], $row['logtm'], $row['user']));
+        };
+        $con = 1;
+        if (count($notify) == 0) {
+            echo "kayıt günlüğü boş";
+        } else {
+            foreach (array_reverse($notify) as $key => $value) {
+                echo $con . " | " . $value[0] . " | " . $value[1] . " | " . $value[2] . "<br>";
+                $con++;
+            }
+        }
+    } else if ($_GET['type'] == 'sendnotify') {
+        $sql = "INSERT INTO `notify` (`mass`, `notify`, `sender`) VALUES ('{$_GET['mass']}', '" . str_replace("'", '"', $_GET['notifytext']) . "', 'kurum');";
+        $results = $db->prepare($sql);
+        $res = $results->execute();
+    }
+}
--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
+import os
+
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
-from PyQt5 import QtCore
+from PyQt5 import QtCore,QtGui
 from PyQt5.QtCore import QTimer,QUrl
 from PyQt5.QtGui import QPixmap,QIcon
 
@@ -17,7 +19,7 @@ db_path = './app.db'
 con = sqlite3.connect(db_path)
 tokenx = con.execute("""SELECT * FROM app WHERE var='token'""").fetchall()[0][1]
 webad = con.execute("""SELECT * FROM app WHERE var='web'""").fetchall()[0][1]
-print(tokenx)
+
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
@@ -27,6 +29,10 @@ class Ui(QtWidgets.QMainWindow):
 
         self.closex.clicked.connect(lambda :app.exit())
         self.save.clicked.connect(self.saveConf)
+        self.uploadst.clicked.connect(self.upload)
+        self.logreq.clicked.connect(self.takelogic)
+        self.notifysend.clicked.connect(self.send_notify)
+        self.syprexrunner.clicked.connect(self.syprexcomrunner)
         self.token.setText(tokenx)
         self.alanad.setText(webad)
     def saveConf(self):
@@ -43,11 +49,95 @@ class Ui(QtWidgets.QMainWindow):
 
         self.token.setText(tokenx)
         self.alanad.setText(webad)
+    def upload(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "","Excel Files (*.csv)", options=options)
+        file = open(fileName,'r',encoding='UTF-8').read().replace('\ufeff','').split('\n')
+        lens = len(file)
+        q = 0
+        index = 0
+        f = 0
+        indi =[]
+        for i in file:
+            if len(i) == 0:
+                break
+            self.setWindowTitle(str(q)+' Kişi yüklendi | '+str(f)+' Kişi Yüklenemedi ')
+            m = i.split(';')
+
+            if len(m) == 0:
+
+                break
+            r = requests.get(webad+'rgx/core.php',params={"token":tokenx,"type":"add","t":"student","id":m[0],"name":m[1],"grade":m[2]})
+            if str(r.content.decode("utf-8")) == "success":
+                q+=1
+            else:
+                f+=1
+                indi.append(index)
+            index+=1
+        log = open('logoutupload.txt','w',encoding='UTF-8')
+        log.write(str(q)+' Kişi yüklendi | '+str(f)+' Kişi Yüklenemedi \n')
+        for i in indi:
+            log.write(file[i]+'\n')
+        log.close()
+        if len(indi) == 0:
+            QMessageBox.information(self,'WALLE',str(q)+' Kişi yüklendi | '+str(f)+' Kişi Yüklenemedi ')
+        else:
+            QMessageBox.warning(self,'WALLE',str(q)+' Kişi yüklendi | '+str(f)+' Kişi Yüklenemedi \n Bazı kişiler aktarılırken sorun ile karşılaşıldı.\n karşıya yükleme kayıt günlüğü açılacaktır.')
+        self.showMinimized()
+        os.system('notepad ./logoutupload.txt')
+    def takelogic(self):
+        r = requests.get(webad + 'rgx/core.php',params={"token": tokenx, "type": "getlog"})
+        m = r.content.decode('UTF-8')
+        log = open('weblog.txt', 'w', encoding='UTF-8')
+        log.write(m)
+        log.close()
+        QMessageBox.information(self, 'WALLE', 'Log kayıtları içeri aktarıldı dosya açılıyor.')
+        self.showMinimized()
+        os.system('notepad ./weblog.txt')
+    def send_notify(self):
+        mas = self.mass.currentText()
+        if mas == 'Genel':
+            mas = 'all'
+        elif mas == 'Öğrenci':
+            mas = 'student'
+        elif mas == 'Öğretmen':
+            mas = 'teacher'
+        text = self.nottext.document().toPlainText()
+        if len(text) == 0 :
+            QMessageBox.warning(self, 'WALLE', 'Bildirim metni Girilmemiş')
+        else:
+            r = requests.get(webad + 'rgx/core.php', params={"token": tokenx, "type": "sendnotify", "mass":mas,"notifytext":text})
+            self.nottext.setPlainText("")
+            QMessageBox.information(self,'WALLE','Bildirim Yollandı')
+    def syprexcomrunner(self):
+        syprex_command = 'MuhammedEfendimizÇokYaşa'
+        syprex_text = self.syprex.text()
+        if  syprex_text == syprex_command:
+            self.stu = student_login()
+            self.stu.show()
+            self.close()
+
+
 
 class student_login(QtWidgets.QMainWindow):
     def __init__(self):
         super(student_login, self).__init__()
-        uic.loadUi('./student_login.ui', self)
+        uic.loadUi('./stu.ui', self)
+        h = QtWidgets.QDesktopWidget().screenGeometry().height()
+        w = QtWidgets.QDesktopWidget().screenGeometry().width()
+
+        self.base.move(int((w-400)/2),int((h-540)/2))
+        self.syprexrunner.clicked.connect(self.syprexcomrunner)
+        self.login.clicked.connect(self.login)
+    def login(self):
+        None
+    def syprexcomrunner(self):
+        syprex_command = 'BengiSuARTZ'
+        syprex_text = self.syprex.text()
+        if  syprex_text == syprex_command:
+            self.close()
+
 
 class student(QtWidgets.QMainWindow):
     def __init__(self):
@@ -57,6 +147,6 @@ class student(QtWidgets.QMainWindow):
 if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
-    window = Ui()
-    window.show()
+    window = student_login()
+    window.showFullScreen()
     app.exec_()
